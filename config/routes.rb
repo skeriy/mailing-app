c4e3cf660b7ff04fd17efb5226c522bcc63eaa5d
Sergey <seergoo117@gmail.com>
2018-06-14 01:13:36 +0300

    patch scss

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :admins, path: 'admins', controllers: { sessions: "admins/sessions", registrations: "admins/registrations"}
   devise_for :users, path: 'users', controllers: { sessions: "users/sessions"}
   resources :blogs
-  
+  resources :bootstrsp
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
 
